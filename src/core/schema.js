649b41eadc60ecc7c4798ebbf38f5531d910a0f4
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-11-09 12:38:57 +0300

    Fixes

--- src/core/schema.js
+++ src/core/schema.js
@@ -7,13 +7,13 @@ function JsonSchema(schema) {
     this.schema = schema;
 }
 JsonSchema.prototype.resolve = function () {
-    const tores = {};
-    function iterator(obj, pkey) {
+    const globres = {};
+    function iterator(obj, parent, pkey) {
         return Promise.all(Object.entries(obj).map(function (t) {
             if (t[0] === '$ref') {
                 const ref = new JsonRef(t[1]);
                 if (ref.url === null) {
-                    tores[pkey] = ref;
+                    parent[pkey] = ref;
                     return [];
                 } else {
                     return fetch(ref.url).then(function (resp) {
@@ -22,17 +22,19 @@ JsonSchema.prototype.resolve = function () {
                         }
                         return resp.json();
                     }).then(function (json) {
-                        return iterator(ref.match(json)).then(function (r) {
+                        return iterator(ref.match(json), parent, pkey).then(function (r) {
                             return Promise.resolve(Object.entries(r));
                         });
                     });
                 }
             } else {
+                const toref = (pkey === null) ? parent : parent[pkey] = {};
+
                 if ((typeof t[1]) === 'object') {
                     if (Array.isArray(t[1])) {
                         return Promise.all(t[1].map(function (e) {
                             if ((typeof e) === 'object') {
-                                return iterator(e, t[0]);
+                                return iterator(e, toref, t[0]);
                             } else {
                                 return Promise.resolve(e);
                             }
@@ -40,7 +42,7 @@ JsonSchema.prototype.resolve = function () {
                             return Promise.resolve([[t[0], r]]);
                         });
                     } else {
-                        return iterator(t[1], t[0]).then(function (r) {
+                        return iterator(t[1], toref, t[0]).then(function (r) {
                             return Promise.resolve([[t[0], r]]);
                         });
                     }
@@ -58,31 +60,39 @@ JsonSchema.prototype.resolve = function () {
             return Promise.resolve(result);
         });
     }
-    return iterator(this.schema, null).then(function (r) {
+    return iterator(this.schema, globres, null).then(function (r) {
         const resolved = [];
-        function proc(v) {
-            if (resolved.indexOf(v) >= 0) {
-                throw new Error("circular reference detected");
+        function prociter(tores, parent) {
+            function proc(v) {
+                if (resolved.indexOf(v) >= 0) {
+                    throw new Error("circular reference detected");
+                }
+                resolved.push(v);
+                const parts = tores[v].parts;
+                if (parts.length > 0 && parts[0] in tores) {
+                    proc(parts[0]);
+                    parent[v] = tores[v].match(r);
+                    delete tores[v];
+                } else {
+                    parent[v] = tores[v].match(r);
+                    delete tores[v];
+                }
             }
-            resolved.push(v);
-            const parts = tores[v].parts;
-            if (parts.length > 0 && parts[0] in tores) {
-                proc(parts[0]);
-                r[v] = tores[v].match(r);
-                delete tores[v];
-            } else {
-                r[v] = tores[v].match(r);
-                delete tores[v];
-            }
-        }
-        while (Object.entries(tores).length > 0) {
-            for (const v in tores) {
-                if (tores.hasOwnProperty(v)) {
-                    proc(v);
-                    break;
+            while (Object.entries(tores).length > 0) {
+                for (const v in tores) {
+                    if (tores.hasOwnProperty(v)) {
+                        if (tores[v] instanceof JsonRef) {
+                            proc(v);
+                        } else {
+                            prociter(tores[v], parent[v]);
+                            delete tores[v];
+                        }
+                        break;
+                    }
                 }
             }
         }
+        prociter(globres, r);
         return Promise.resolve(r);
     });
 };
